f0209ace5dd467f3acb9452f6626f231732ef01c
Eke Victor Chibuike <ekevictor216@gmail.com>
2026-08-07 12:15:01 +0100

    Update API URL for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://connecticut-bottom-north-encourages.trycloudflare.com",
 });
 
 export default api;
