0676400e041fae43566303c8894f5f0d7fec6e71
Eke Victor Chibuike <ekevictor216@gmail.com>
2026-09-10 15:16:11 +0100

    Connect to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const productRoutes = require("./routes/productRoutes");
 
 require("dotenv").config();
 
+const connectDB = require("./config/db");
 const express = require("express");
 const cors = require("cors");
 
@@ -10,7 +11,7 @@ const chatRoutes = require("./routes/chatRoutes");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Enable CORS
 app.use(cors());
@@ -27,6 +28,7 @@ app.get("/", (req, res) => {
   res.send("Elite Devices Backend is Running!");
 });
 
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
